7143e8469268bc12789764a7b570371852e95040
Jacob Hamm <jdhamm17@hotmail.com>
2024-03-05 09:14:40 -0500

    Adding CircleCI config file test1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def Add(a, b):
         return a + b
         
 def SayHello():
+        assert Add(2,3) == 6
+        assert Add(5,5) == 10
         print("Hello CIS 411")
 
 if __name__ == '__main__':
